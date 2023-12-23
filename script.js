7baa896f12a8b76ecddff5e0e1f8e68d413a3ed8
vikrant-04 <vikrantyadav4802@gmail.com>
2023-12-23 11:09:56 +0530

    added error message for wrong input city and hide the result visibility

--- script.js
+++ script.js
@@ -9,36 +9,44 @@ var icon=document.querySelector(".icon")
 async function weather(city){
     const response=await fetch(api_url+`&q=${city}`+`&appid=${v1+v2}`);
     var data= await response.json();
-    console.log(data);
-    if(data.weather[0].main=="Clouds"){
-        icon.src="images/clouds.png";
+    if(response.status==404) {
+        document.querySelector(".error").style.visibility="visible";
+        document.querySelector(".result").style.display="none";
     }
-    else if(data.weather[0].main=="Clear"){
-        icon.src="images/clear.png";
+    else{
+        document.querySelector(".error").style.visibility="hidden";
+        document.querySelector(".result").style.display="block";
+        if(data.weather[0].main=="Clouds"){
+            icon.src="images/clouds.png";
+        }
+        else if(data.weather[0].main=="Clear"){
+            icon.src="images/clear.png";
+        }
+        else if(data.weather[0].main=="Clouds"){
+            icon.src="images/clouds.png";
+        }
+        else if(data.weather[0].main=="Rain"){
+            icon.src="images/rain.png";
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            icon.src="images/drizzle.png";
+        }
+        else if(data.weather[0].main=="Mist"){
+            icon.src="images/mist.png";
+        }
+        else if(data.weather[0].main=="Snow"){
+            icon.src="images/snow.png";
+        }
+        
+        document.querySelector(".temp-val").innerHTML=Math.round(data.main.temp)+"°c";
+        document.querySelector(".temperature").innerHTML=city;
+        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+        document.querySelector(".wind-speed").innerHTML=data.wind.speed+"k/h";
+        
     }
-    else if(data.weather[0].main=="Clouds"){
-        icon.src="images/clouds.png";
-    }
-    else if(data.weather[0].main=="Rain"){
-        icon.src="images/rain.png";
-    }
-    else if(data.weather[0].main=="Drizzle"){
-        icon.src="images/drizzle.png";
-    }
-    else if(data.weather[0].main=="Mist"){
-        icon.src="images/mist.png";
-    }
-    else if(data.weather[0].main=="Snow"){
-        icon.src="images/snow.png";
-    }
-    
-    document.querySelector(".temp-val").innerHTML=Math.round(data.main.temp)+"°c";
-    document.querySelector(".temperature").innerHTML=city;
-    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-    document.querySelector(".wind-speed").innerHTML=data.wind.speed+"k/h";
-    
 }
-
-btn.addEventListener("click",()=>{
-weather(search.value)
-})
+    
+    btn.addEventListener("click",()=>{
+        weather(search.value)
+    })
+    
